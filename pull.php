04085a8796b8bb5fc3241aaee2f6d752d32851f0
persist8 <727271755@qq.com>
2017-03-27 11:05:07 +0800

    blog

--- pull.php
+++ pull.php
@@ -1,4 +1,4 @@
 <?php
-exec('/bin/bash /home/www/blogtypecho/product.sh', $output, $return_var);
+/* exec('/bin/bash /home/www/blogtypecho/product.sh', $output, $return_var);
 print_r($output);
-print_r($return_var);
+print_r($return_var); */
